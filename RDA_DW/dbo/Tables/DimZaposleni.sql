4dd6ef7359c8f90596a14b8d2861a4baaaa8a31d
AleksandarTodosijevicPrivate <todosijevic.a@yahoo.com>
2019-05-06 00:26:04 +0200

    Adding Dynamic Data Masking on DimZaposleni and grant select permissions on this table for [DELL\SalesPerson]

--- RDA_DW/dbo/Tables/DimZaposleni.sql
+++ RDA_DW/dbo/Tables/DimZaposleni.sql
@@ -1,15 +1,23 @@
 ﻿CREATE TABLE [dbo].[DimZaposleni] (
-    [ZaposleniKey]        INT           NOT NULL,
-    [IzvorniZaposleni_ID] INT           NOT NULL,
-    [ImeZaposlenog]       NVARCHAR (20) NULL,
-    [PrezimeZaposlenog]   NVARCHAR (20) NULL,
-    [JmbgZaposlenog]      CHAR (13)     NULL,
-    [EmailZaposlenog]     CHAR (30)     NULL,
-    [PozicijaZaposlenog]  NVARCHAR (30) NULL,
-    [HashKey]             INT           NULL,
-    [Obrisan]             SMALLDATETIME NULL,
+    [ZaposleniKey]        INT                                            NOT NULL,
+    [IzvorniZaposleni_ID] INT                                            NOT NULL,
+    [ImeZaposlenog]       NVARCHAR (20)                                  NULL,
+    [PrezimeZaposlenog]   NVARCHAR (20)                                  NULL,
+    [JmbgZaposlenog]      CHAR (13) MASKED WITH (FUNCTION = 'default()') NULL,
+    [EmailZaposlenog]     CHAR (30) MASKED WITH (FUNCTION = 'email()')   NULL,
+    [PozicijaZaposlenog]  NVARCHAR (30)                                  NULL,
+    [HashKey]             INT                                            NULL,
+    [Obrisan]             SMALLDATETIME                                  NULL,
     PRIMARY KEY NONCLUSTERED ([ZaposleniKey] ASC)
 );
 
 
+
+
+
+
+GO
+GRANT SELECT
+    ON OBJECT::[dbo].[DimZaposleni] TO [DELL\SalesPerson]
+    AS [dbo];
 
