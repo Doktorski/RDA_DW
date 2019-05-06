8339d9d7a03996508f0002537d37874a99a5fcb9
AleksandarTodosijevicPrivate <todosijevic.a@yahoo.com>
2019-05-06 12:08:27 +0200

    Adding EmailZaposlenog within DimZaposleni

--- RDA_DW/dbo/Views/dbo.Zaposleni.sql
+++ RDA_DW/dbo/Views/dbo.Zaposleni.sql
@@ -1,4 +1,5 @@
-﻿CREATE VIEW dbo.Zaposleni
+﻿
+CREATE VIEW [dbo].[Zaposleni]
 AS
 SELECT 
 	ZaposleniKey AS [Zaposleni Key]
@@ -6,5 +7,6 @@ SELECT
 	,PrezimeZaposlenog AS [Prezime Zaposlenog]
 	,JmbgZaposlenog AS [JMBG Zaposlenog]
 	,PozicijaZaposlenog AS [Pozicija Zaposlenog]
+	,EmailZaposlenog AS [Email Zasposlenog]
 FROM dbo.DimZaposleni
 WHERE Obrisan IS NULL;
